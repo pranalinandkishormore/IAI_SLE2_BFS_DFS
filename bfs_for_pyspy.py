cf59d32dd3304e82e566bc6017934abcd00bf81a
pranalinandkishormore <pranalimore1221@gmail.com>
2026-09-24 01:13:33 -0700

    Define graph structure for BFS in bfs_for_pyspy.py
    Added a graph structure for BFS traversal.

--- bfs_for_pyspy.py
+++ bfs_for_pyspy.py
@@ -1,4 +1,18 @@
-from bfs import bfs, graph
+from bfs import bfs
+
+graph = {
+    'A': ['B', 'I'],
+    'B': ['C', 'D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['G'],
+    'F': ['H'],
+    'G': ['K'],
+    'H': ['K'],
+    'I': ['J'],
+    'J': ['K'],
+    'K': []
+}
 
 for _ in range(100000):
     bfs(graph, 'A', 'B')
