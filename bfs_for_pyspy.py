5ee941c2b8db46ea274c68eac07b2af4781b6585
pranalinandkishormore <pranalimore1221@gmail.com>
2026-09-24 01:53:25 -0700

    Update bfs_for_pyspy.py

--- bfs_for_pyspy.py
+++ bfs_for_pyspy.py
@@ -1,18 +1,4 @@
-from bfs import bfs
-
-graph = {
-    'A': ['B', 'I'],
-    'B': ['C', 'D'],
-    'C': ['E'],
-    'D': ['F'],
-    'E': ['G'],
-    'F': ['H'],
-    'G': ['K'],
-    'H': ['K'],
-    'I': ['J'],
-    'J': ['K'],
-    'K': []
-}
+from bfs import bfs, graph
 
 for _ in range(100000):
     bfs(graph, 'A', 'B')
