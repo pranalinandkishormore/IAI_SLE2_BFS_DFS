05dd443a43e0c44dddab9ff463cc457bd9689388
pranalinandkishormore <pranalimore1221@gmail.com>
2026-09-24 01:14:51 -0700

    Replace DFS calls with BFS in loop

--- dfs_for_pyspy.py
+++ dfs_for_pyspy.py
@@ -1,6 +1,20 @@
-from dfs import dfs, graph
+from bfs import bfs
+
+graph = {
+    'A': ['B', 'I'],
+    'B': ['C', 'D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['G'],
+    'F': ['H'],
+    'G': ['K'],
+    'H': ['K'],
+    'I': ['J'],
+    'J': ['K'],
+    'K': []
+}
 
 for _ in range(100000):
-    dfs(graph, 'A', 'B')
-    dfs(graph, 'A', 'I')
-    dfs(graph, 'A', 'K')
+    bfs(graph, 'A', 'B')
+    bfs(graph, 'A', 'I')
+    bfs(graph, 'A', 'K')
